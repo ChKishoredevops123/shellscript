d9d0bed873ee69fdea13c1a0670651a7a7569f59
Kishore <chkishore.devops123@gmail.com>
2023-08-20 17:46:28 +0530

    changes

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,6 +6,8 @@ PERSON1=$1
 PERSON2=$2
 DATE=$(date)
 
+read="Enter person1 name"
+read="Enter person2 name"
 echo "$PERSON1: Hello $PERSON2"
 echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
