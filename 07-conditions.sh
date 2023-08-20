2a732df03e23d73f3ee00d60bf3b53478a619f2d
Kishore <chkishore.devops123@gmail.com>
2023-08-20 19:10:39 +0530

    retest

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -7,9 +7,8 @@ echo "Please enter your number: "
 if [ $NUMBER -gt 10 ]
     echo " Your number is greater than 10."
 else
-    if [ $NUMBER -lt 10 ]
+   ## if [ $NUMBER -lt 10 ]
         echo "Your number is less than 10."
-    else
-        echo "Your number is equal to 10."
-fi
+  ##  else
+    ##    echo "Your number is equal to 10."
 fi
