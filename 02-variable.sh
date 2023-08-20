fd6af7b893e276bb9324a35853738fcded64e09e
Kishore <chkishore.devops123@gmail.com>
2023-08-20 17:22:36 +0530

    new one

--- 02-variable.sh
+++ 02-variable.sh
@@ -6,7 +6,9 @@ PERSON1=Kishore
 PERSON2=Suresh
 
 echo "$PERSON1: Hi How are you"
+echo "=================================================================="
 echo "$PERSON2: I am good"
-
+echo "=================================================================="
 echo "$PERSON2: How are you $PERSON1"
+echo "=================================================================="
 echo "$PERSON1: i am good $PERSON2"
