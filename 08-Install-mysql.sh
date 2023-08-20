1f49009bbeae22c67f5a48d9d6740243d183501e
Kishore <chkishore.devops123@gmail.com>
2023-08-20 20:19:49 +0530

    super

--- 08-Install-mysql.sh
+++ 08-Install-mysql.sh
@@ -4,14 +4,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
-    echo " Please run with SUDO access:"
-    exit 1
-else 
-    yum install mysql -y
+        echo " Please run with SUDO access:"
+        exit 1
+    else 
+        yum install mysql -y
     if [ $? -ne 0 ]
     then
         echo "ERROR: Installation not successful"
         exit 1
     else
         echo "ERROR: Installation successful"
+    fi
 fi
