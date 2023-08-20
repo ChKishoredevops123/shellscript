dd1f1d650c6f9d837e7d64d88e746f5f6bb4d9fd
Kishore <chkishore.devops123@gmail.com>
2023-08-20 20:23:52 +0530

    nice1

--- 08-Install-mysql.sh
+++ 08-Install-mysql.sh
@@ -10,9 +10,18 @@ then
         yum install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "ERROR: Installation not successful"
+        echo "ERROR: MySQL Installation not successful"
         exit 1
     else
-        echo "INFO: Installation successful"
+        echo "INFO: MySQL Installation successful"
+    fi
+
+         yum install nodejs -y
+    if [ $? -ne 0 ]
+    then
+        echo "ERROR: NODEJS Installation not successful"
+        exit 1
+    else
+        echo "INFO: NODEJS Installation successful"
     fi
 fi
