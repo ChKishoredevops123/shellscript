2ff61ebf70a6ef9c093a8a2d0742f7197793a425
Kishore <chkishore.devops123@gmail.com>
2023-08-20 17:36:24 +0530

    new file

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Hi Friends.."
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good $PERSON1 How are you ???"
