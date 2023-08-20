bee0bf9e95e5c6e84bb7c594df0faf19e39c3235
Kishore <chkishore.devops123@gmail.com>
2023-08-20 17:39:32 +0530

    retest

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,11 @@ echo "$PERSON1: Hello $PERSON2"
 echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good $PERSON1 How are you ???"
+echo "
+
+"
+echo "$PERSON1: What is date and time ??"
+
+DATE=$(date)
+
+echo "$PERSON: today date is : $DATE"
