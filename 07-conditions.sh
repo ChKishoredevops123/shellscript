ed38b7713e132df7c4942a65b2b33c6ee9520591
Kishore <chkishore.devops123@gmail.com>
2023-08-20 19:07:49 +0530

    conditions

---
+++ 07-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+echo "Please enter your number: "
+
+if [ $NUMBER -gt 10 ]
+    echo " Your number is greater than 10."
+else
+    if [ $NUMBER -lt 10 ]
+        echo "Your number is less than 10."
+    else
+        echo "Your number is equal to 10."
