9548606e3f4ee11793ec159545807d41f08b47b6
Kishore <chkishore.devops123@gmail.com>
2023-08-20 18:02:05 +0530

    newonew

---
+++ 05-Readcommand.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please enter your username:"
+
+read USERNAME
+
+echo "your username is :$USERNAME"
+
+echo "Please enter your Password:"
+
+read PASSWORD
+
+echo "your Password is :$PASSWORD"
