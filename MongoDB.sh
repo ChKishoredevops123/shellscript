6fb991f9f973d57d295ec5fa02c7267a73f4f723
Kishore <chkishore.devops123@gmail.com>
2023-08-26 15:07:22 +0530

    install

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
 echo "hello mongoDB"
 
-yum install nginx
+yum install nginx -y
+RESULT=Echo $?
+
+If [ $RESULT -ne 0 ]
+then
+    echo "ERROR: Installation not successful "
+else
+    echo "INFO: Installation successful "
+fi
+
+
+
