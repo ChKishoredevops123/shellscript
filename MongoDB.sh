778f296a15e40cb3767b25d8c7ee69362c4d1c19
Kishore <chkishore.devops123@gmail.com>
2023-08-26 15:28:53 +0530

    new

--- MongoDB.sh
+++ MongoDB.sh
@@ -5,10 +5,12 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
             echo -e "$1 Install Failure $N"
+            exit 1
         else
             echo -e "$1 Install Success $N"
     fi
 }
-yum install nginx -y
+yum install nginxx -y
 VALIDATE $? "Installing Nginx"
+
 
