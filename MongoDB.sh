b0a4a35b0d5791125eb1d24c7ea4f81dd2757993
Kishore <chkishore.devops123@gmail.com>
2023-08-26 15:16:25 +0530

    test

--- MongoDB.sh
+++ MongoDB.sh
@@ -4,11 +4,12 @@
 echo "hello mongoDB"
 
 yum install nginx -y
-RESULT=Echo $?
+RESULT=$(echo $?)
 
 If [ $RESULT -ne 0 ]
 then
     echo "ERROR: Installation not successful "
+    exit 1
 else
     echo "INFO: Installation successful "
 fi
